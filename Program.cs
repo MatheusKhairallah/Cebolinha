6da0333353c40e9aaeda333a51f39eeec3ce5187
matheus <playermaster10like@gmail.com>
2020-03-16 18:42:58 -0300

    programa c#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite algo: ");
+            string texto=Console.ReadLine();
+            string texto2=texto.Replace("r", "l");
+
+            Console.Clear();
+            
+            string mensagem="" +texto2+ "";
+
+            Console.WriteLine(mensagem);
+        }
+    }
+}
